e47b0035e7abcf29ef81c6981c3adfb7a97eecbc
Fernanda <fernandamora88@gmail.com>
2017-03-13 14:11:05 -0700

    add redirect to homepage when already logged in'

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,15 +4,27 @@ import { Router, Route, hashHistory } from 'react-router';
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App } >
-        <Route path="/signup" component={ SessionFormContainer }/>
-        <Route path="/login" component={ SessionFormContainer }/>
-      </Route>
-    </Router>
-  </Provider>
-);
+
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    if(store.getState().session.currentUser) {
+      replace('/');
+    }
+  }
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App } >
+          <Route path="/signup" onEnter= {_redirectIfLoggedIn} component={ SessionFormContainer }/>
+          <Route path="/login" onEnter= {_redirectIfLoggedIn} component={ SessionFormContainer }/>
+        </Route>
+      </Router>
+    </Provider>
+  );
+
+
+};
 
 export  default Root;
