f543e30182082eeda5b6278fe8c17fe7bc8c76d6
Fernanda <fernandamora88@gmail.com>
2017-03-13 12:11:48 -0700

    persist logged in user

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -5,11 +5,20 @@ import Root from './components/root';
 import { login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    const store = configureStore();
-    ReactDOM.render(<Root store={ store }/>, root);
 
-    window.store = store;
-    window.login = login;
-    window.logout = logout;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
+
+  window.store = store;
+  window.login = login;
+  window.logout = logout;
 });
